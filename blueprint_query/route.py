2a1238d9a496e41795a17950c63396ba9cadf3d3
NovokshanovEvgeny <evgeny.novokshanov@gmail.com>
2022-10-27 21:58:57 +0300

    №2

--- blueprint_query/route.py
+++ blueprint_query/route.py
@@ -1,13 +1,11 @@
-import os  # работа с объектами операционной системы
-
-from flask import Blueprint, request, render_template, current_app  # глобальная переменная с конфигом app
+import os
+from flask import Blueprint, request, render_template, current_app
 from db_work import select
 from sql_provider import SQLProvider
 
+blueprint_query = Blueprint('bp_query', __name__, template_folder='templates')
 
-blueprint_query = Blueprint('bp_query', __name__, template_folder='templates')  # создание blueprint'а
-
-provider = SQLProvider(os.path.join(os.path.dirname(__file__), 'sql'))  # создание словаря для текущего blueprint'а
+provider = SQLProvider(os.path.join(os.path.dirname(__file__), 'sql'))
 
 
 @blueprint_query.route('/queries', methods=['GET', 'POST'])
@@ -18,7 +16,10 @@ def queries():
         input_product = request.form.get('product_name')
         if input_product:
             _sql = provider.get('product.sql', input_product=input_product)
-            product_result, schema = select(current_app.config['dbconfig'], _sql)
-            return render_template('db_result.html', schema=schema, result=product_result)
+            product_result, schema = select(current_app.config['db_config'], _sql)
+            return render_template('db_result.html', schema=schema, result=product_result )
         else:
             return "Repeat input"
+
+
+
--- db_work.py
+++ db_work.py
@@ -1,26 +1,45 @@
-from typing import Tuple, List
 from db_context_manager import DBContextManager
 
 
-def select(db_config: dict, sql: str) -> Tuple[Tuple, List[str]]:
-    """
-    Выполняет запрос (SELECT) к БД с указанным конфигом и запросом.
-    Args:
-        db_config: dict - Конфиг для подключения к БД.
-        sql: str - SQL-запрос.
-    Return:
-        Кортеж с результатом запроса и описанеим колонок запроса.
-    """
-    result = tuple()
-    schema = []
-    with DBContextManager(db_config) as cursor:
+def select(dbconfig: dict, _sql: str):
+    with DBContextManager(dbconfig) as cursor:
+
         if cursor is None:
-            raise ValueError('Cursor not found')
-        cursor.execute(sql)#Метод cursor.execute() выполняет команду SQL.
-        # Команды SQL могут быть параметризованными, то есть передаются заполнители вместо литералов SQL.
-        schema = [column[0] for column in cursor.description]#Предоставляет имена столбцов последнего запроса.
-        # Чтобы оставаться совместимым с API-интерфейсом Python DB,
-        # он возвращает кортеж из 7 элементов для каждого столбца,
-        # где последние шесть элементов каждого кортежа равны None.
-        result = cursor.fetchall()#выбирает все оставшиеся строки результата запроса, возвращая список.
+            raise ValueError('Курсор не создан')
+
+        cursor.execute(_sql)
+        schema = [column[0] for column in cursor.description]
+        result = cursor.fetchall()
     return result, schema
+
+
+def select_dict(dbconfig: dict, _sql:str):
+    with DBContextManager(dbconfig) as cursor:
+
+        if cursor is None:
+            raise ValueError('Курсор не создан')
+
+        cursor.execute(_sql)
+        result = []
+        schema = [column[0] for column in cursor.description]
+        for row in cursor.fetchall():
+            result.append(dict(zip(schema, row))) #мы получаем наименования колонок и содержимое
+            # -> переводим их с помощью zip в список кортежей из двух соответствующих элементов
+            # -> переводим с помощью dict в словарь с соответствующими ключами
+
+
+
+        print('result_dict=', result)
+    return result
+
+
+def call_proc(dbconfig: dict, proc_name: str, *args):
+    with DBContextManager(dbconfig) as cursor:
+        if cursor is None:
+            raise ValueError('Курсор не создан')
+        param_tuple = []
+        for arg in args:
+            param_tuple.append(arg)
+        print(param_tuple)
+        res = cursor.callproc(proc_name, param_tuple)
+    return res
